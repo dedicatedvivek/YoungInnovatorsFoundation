72122ecb9636d7f9873cf9fdcabe2d7613b10b92
mehtadhiraj <mehtadhiraj21@gmail.com>
2019-03-17 05:09:02 +0530

    Landing page done | template integrated to members | Bsic styling remaining

--- resources/views/members/assign.blade.php
+++ resources/views/members/assign.blade.php
@@ -28,6 +28,27 @@
 	<link rel="stylesheet" type="text/css" href="/ContactFrom_v5/css/util.css">
 	<link rel="stylesheet" type="text/css" href="/ContactFrom_v5/css/main.css">
 <!--===============================================================================================-->
+	<link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Overpass:300,400,400i,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="/colrib/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="/colrib/css/animate.css">
+    
+    <link rel="stylesheet" href="/colrib/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/colrib/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/colrib/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="/colrib/css/aos.css">
+
+    <link rel="stylesheet" href="/colrib/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="/colrib/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="/colrib/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="/colrib/css/flaticon.css">
+    <link rel="stylesheet" href="/colrib/css/icomoon.css">
+    <link rel="stylesheet" href="/colrib/css/style.css">
 
 <style type="text/css">
 	#sub-type{
@@ -36,6 +57,37 @@
 </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="/">YIFNGO</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="/about" class="nav-link">About</a></li>
+          <li class="nav-item"><a href="/causes" class="nav-link">Causes</a></li>
+          <li class="nav-item"><a href="/donate" class="nav-link">Donate</a></li>
+          <li class="nav-item"><a href="/gallery" class="nav-link">Gallery</a></li>
+          <li class="nav-item"><a href="/contact" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="hero-wrap" style="background-image: url('/colrib/images/bg_7.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
+          <div class="col-md-7 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
+             <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="/">Home</a></span> <span>Home</span></p>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Home</h1>
+          </div>
+        </div>
+      </div>
+    </div>
 
 <div class="row container-contact100">
 	<div class="col-sm-6">
@@ -171,6 +223,24 @@
 			}});
 		}
 	</script>
+	<script src="/colrib/js/jquery.min.js"></script>
+  <script src="/colrib/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="/colrib/js/popper.min.js"></script>
+  <script src="/colrib/js/bootstrap.min.js"></script>
+  <script src="/colrib/js/jquery.easing.1.3.js"></script>
+  <script src="/colrib/js/jquery.waypoints.min.js"></script>
+  <script src="/colrib/js/jquery.stellar.min.js"></script>
+  <script src="/colrib/js/owl.carousel.min.js"></script>
+  <script src="/colrib/js/jquery.magnific-popup.min.js"></script>
+  <script src="/colrib/js/aos.js"></script>
+  <script src="/colrib/js/jquery.animateNumber.min.js"></script>
+  <script src="/colrib/js/bootstrap-datepicker.js"></script>
+  <script src="/colrib/js/jquery.timepicker.min.js"></script>
+  <script src="/colrib/js/scrollax.min.js"></script>
+  <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> -->
+  <script src="/colrib/js/google-map.js"></script>
+  <script src="/colrib/js/main.js"></script>
+  
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/memberhome', function () {
-    return view('members.home');
-});
-
 
 Route::get('/newstakeholder', function () {
     return view('new_stakeholder');
@@ -38,15 +30,43 @@ Route::post("/insert_stakeholder","InsertController@insert_stakeholder");
 
 // Routes for members
 
+Route::get('/memberhome', function () {
+    return view('members.home');
+});
+
 Route::get('/members/assign', "InsertController@select_ovrelations");
 Route::post('/members/assign', "InsertController@insert_ovrelations");
 
-Route::get('/members/add', function(){
-    return view('members.addmember');
-});
+Route::get('/members/add', function(){return view('members.addmember');});
+Route::post('/members/add', "InsertController@insert_member");
 
 Route::get('/members/fetch_ovrelations', "InsertController@fetch_ovrelations");
 
-Route::post('/members/add', "InsertController@insert_member");
-
 // members routes end
+
+/* =========================================== 
+    Static Routes
+==============================================*/
+Route::get('/', function () {
+    return view('static.index');
+});
+
+Route::get('/about', function () {
+    return view('static.about');
+});
+
+Route::get('/contact', function () {
+    return view('static.contact');
+});
+
+Route::get('/causes', function () {
+    return view('static.causes');
+});
+
+Route::get('/gallery', function () {
+    return view('static.gallery');
+});
+
+Route::get('/contact', function () {
+    return view('static.contact');
+});
