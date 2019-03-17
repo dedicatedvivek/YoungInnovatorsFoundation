2bae75b2f3645314fb1994bf666a064301bffacc
karthikmudaliar <karthik.mudaliar16@siesgst.ac.in>
2019-03-17 10:14:24 +0530

    volunteers done completely

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::get('/volunteerhome', function () {
     return view('volunteers.home');
 });
 
+Route::get('/studenthome', function () {
+    return view('students.home');
+});
+
 Route::get('/neworganization', function () {
     return view('new_organization');
 });
