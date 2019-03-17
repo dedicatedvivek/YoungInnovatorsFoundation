99e1715375025968648f9ef38402e95cc37da98b
unknown <bhavinfriend65@gmail.com>
2019-03-17 11:01:19 +0530

    certificate added

--- resources/views/members/homenew.blade.php
+++ resources/views/members/homenew.blade.php
@@ -233,7 +233,7 @@ body,html{
 
 	 <?php
 
-  $select_query= App\Organizations::select('registration_numbers','o_names','o_types','o_addresses','contact_nos')->get();
+  $select_query= App\Organizations::select('registration_numbers','o_names','o_types','o_addresses','contact_nos','file_names')->get();
   
 
   $count_arr = count($select_query);
@@ -299,6 +299,7 @@ $count_volunteer = count($volunteer_query);
           $o_types = $list['o_types'];
           $o_addresses = $list['o_addresses'];
           $contact_nos = $list['contact_nos'];
+          $file_names = $list['file_names'];
 
 
 
@@ -325,6 +326,12 @@ $count_volunteer = count($volunteer_query);
           echo "<td>";
           echo $contact_nos;
           echo "</td>";
+
+
+          echo "<td>";
+          echo "<a href = '$file_names' >Certificate</a>";
+          echo "</td>";
+
 
           echo "</td>";
 
@@ -434,6 +441,7 @@ $count_volunteer = count($volunteer_query);
         <th>Address</th>
         <th>Contact Number</th>
         <th>Date Of Birth</th>
+        <th>certificate</th>
       </tr>
       </thead>
     
@@ -450,7 +458,6 @@ $count_volunteer = count($volunteer_query);
           $addresses3 = $list3['addresses'];
           $job_types3 = $list3['job_types'];
           $contact_nos3 = $list3['contact_nos'];
-          
           $dobs3 = $list3['dobs'];
 
 
@@ -489,6 +496,8 @@ $count_volunteer = count($volunteer_query);
           echo "<td>";
           echo $dobs3;
           echo "</td>";
+
+          
 
           echo "</td>";
 
